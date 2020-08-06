14b19836ff09b6e9253c1e2db1b42dab3b0a21c5
happy wang <shuizhongyueming@gmail.com>
2020-08-06 17:20:31 +0800

    rm build/

--- build/_dist_/index.css.proxy.js
+++ build/_dist_/index.css.proxy.js
@@ -1,14 +1,14 @@
-const code=`body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-    monospace;
-}
+const code=`body {\r
+  margin: 0;\r
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",\r
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",\r
+    sans-serif;\r
+  -webkit-font-smoothing: antialiased;\r
+  -moz-osx-font-smoothing: grayscale;\r
+}\r
+\r
+code {\r
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",\r
+    monospace;\r
+}\r
 `,styleEl=document.createElement("style"),codeEl=document.createTextNode(code);styleEl.type="text/css",styleEl.appendChild(codeEl),document.head.appendChild(styleEl);
