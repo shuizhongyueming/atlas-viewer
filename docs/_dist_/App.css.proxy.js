9f2dac0d605a7981f9ef3e8c53c35bb7c17d0dcd
happy wang <shuizhongyueming@gmail.com>
2020-08-06 17:25:18 +0800

    update

--- docs/_dist_/App.css.proxy.js
+++ docs/_dist_/App.css.proxy.js
@@ -1,15 +1,15 @@
-const code=`.App {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  overflow: hidden;
-}
-
-.content {
-  display: flex;
-  flex-direction: row;
-  flex: 100px 1 1;
-  align-items: stretch;
-  overflow: hidden;
-}
+const code=`.App {\r
+  display: flex;\r
+  flex-direction: column;\r
+  height: 100vh;\r
+  overflow: hidden;\r
+}\r
+\r
+.content {\r
+  display: flex;\r
+  flex-direction: row;\r
+  flex: 100px 1 1;\r
+  align-items: stretch;\r
+  overflow: hidden;\r
+}\r
 `,styleEl=document.createElement("style"),codeEl=document.createTextNode(code);styleEl.type="text/css",styleEl.appendChild(codeEl),document.head.appendChild(styleEl);
