752fe289468bd58629d6dc7059956ced66a810cd
happy wang <shuizhongyueming@gmail.com>
2022-11-21 15:47:55 +0800

    Update snowpack.config.js
    change baseUrl

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = process.env;
 const buildOptions = {};
 if (NODE_ENV === 'production') {
-  buildOptions.baseUrl = 'https://www.shuizhongyueming.com/atlas-viewer/';
+  buildOptions.baseUrl = 'https://shuizhongyueming.com/atlas-viewer/';
 }
 module.exports = {
   extends: '@snowpack/app-scripts-react',
